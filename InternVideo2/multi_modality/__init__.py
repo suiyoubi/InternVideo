9f0bc54d09521236670c3808795bb4c801d3a69e
Ao Tang <aot@nvidia.com>
2025-08-21 09:33:02 -0700

    Add model imports and update __all__ in multi_modality init file; simplify FlashAttention import
    Signed-off-by: Ao Tang <aot@nvidia.com>

--- InternVideo2/multi_modality/__init__.py
+++ InternVideo2/multi_modality/__init__.py
@@ -1,1 +1,20 @@
 
+# Import main models
+from .models.internvideo2_stage2_visual import InternVideo2_Stage2_visual
+from .models.internvideo2_stage2_audiovisual import InternVideo2_Stage2_audiovisual
+from .models.internvideo2_clip import InternVideo2_CLIP
+from .models.internvideo2_clip_small import InternVideo2_CLIP_small
+
+# Import backbone components
+from .models.backbones.internvideo2 import pretrain_internvideo2_1b_patch14_224, pretrain_internvideo2_6b_patch14_224
+from .models.backbones.bert.builder import build_bert
+
+__all__ = [
+    'InternVideo2_Stage2_visual',
+    'InternVideo2_Stage2_audiovisual', 
+    'InternVideo2_CLIP',
+    'InternVideo2_CLIP_small',
+    'pretrain_internvideo2_1b_patch14_224',
+    'pretrain_internvideo2_6b_patch14_224',
+    'build_bert'
+]
