d0fcf8d5d60a878e2ad779cce04e488e6c4c267d
Ao Tang <aot@nvidia.com>
2025-08-22 06:55:47 -0700

    Add interpolate_pos_embed_internvideo2_new to __all__ in multi_modality init file
    Signed-off-by: Ao Tang <aot@nvidia.com>

--- InternVideo2/multi_modality/__init__.py
+++ InternVideo2/multi_modality/__init__.py
@@ -8,12 +8,14 @@ from .models.internvideo2_clip_small import InternVideo2_CLIP_small
 # Import backbone components
 from .models.backbones.internvideo2 import pretrain_internvideo2_1b_patch14_224, pretrain_internvideo2_6b_patch14_224
 from .models.backbones.bert.builder import build_bert
+from .models.backbones.internvideo2.pos_embed import interpolate_pos_embed_internvideo2_new
 
 __all__ = [
     'InternVideo2_Stage2_visual',
     'InternVideo2_Stage2_audiovisual', 
     'InternVideo2_CLIP',
     'InternVideo2_CLIP_small',
+    'interpolate_pos_embed_internvideo2_new',
     'pretrain_internvideo2_1b_patch14_224',
     'pretrain_internvideo2_6b_patch14_224',
     'build_bert'
